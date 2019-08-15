abdd2c4fec5be951797e862bffcca3a0276acb0b
J-Rios <jrios.github@gmail.com>
2019-08-15 14:53:24 +0200

    Fix captcha directory path. Modify to a more readable constants file.

--- sources/constants.py
+++ sources/constants.py
@@ -10,9 +10,9 @@ Author:
 Creation date:
     09/09/2018
 Last modified date:
-    14/08/2019
+    15/08/2019
 Version:
-    1.5.1
+    1.5.2
 '''
 
 ####################################################################################################
@@ -25,37 +25,82 @@ from os import path
 
 ### Constants ###
 
-SCRIPT_PATH = path.dirname(path.realpath(__file__)) # Actual constant.py full path directory name
+# Actual constants.py full path directory name
+SCRIPT_PATH = path.dirname(path.realpath(__file__))
 
+
+# General Bots Parameters
 CONST = {
-    "TOKEN" : "XXXXXXXXX:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX", # Bot Token (get it from @BotFather)
-    "LANG_DIR" : SCRIPT_PATH + "/language", # Languages texts files directory path
-    "DATA_DIR" : SCRIPT_PATH + "/data", # Data directory path
-    "CHATS_DIR" : SCRIPT_PATH + "/data/chats", # Chats directory path
-    "CAPTCHAS_DIR" : "./data/captchas", # Directory where create/generate temporary captchas images
-    "F_CONF" : "configs.json", # Chat configurations JSON files name
-    "INIT_TITLE" : "Unknown Chat", # Initial chat title at Bot start
-    "INIT_LINK" : "Unknown", # Initial chat link at Bot start
-    "INIT_LANG" : "EN", # Initial language at Bot start
-    "INIT_ENABLE" : True, # Initial enable/disable status at Bot start
-    "INIT_CAPTCHA_TIME_MIN" : 5, # Initial captcha solve time (in minutes)
-    "INIT_CAPTCHA_DIFFICULTY_LEVEL" : 2, # Initial captcha difficult level
-    "INIT_CAPTCHA_CHARS_MODE" : "nums", # Initial captcha characters mode (nums, hex or ascci)
-    "T_DEL_MSG" : 5, # Default time (in mins) to remove self-destruct sent messages from the Bot
-    "F_TLDS" : "tlds-alpha-by-domain.txt", # IANA TLD list (https://data.iana.org/TLD/tlds-alpha-by-domain.txt)
-    "REGEX_URLS" : r"((?<=[^a-zA-Z0-9])*(?:https\:\/\/|[a-zA-Z0-9]{{1,}}\.{{1}}|\b)(?:\w{{1,}}\.{{1}}){{1,5}}(?:{})\b/?(?!@))",
-    "DEVELOPER" : "@JoseTLG", # Bot developer
-    "REPOSITORY" : "https://github.com/J-Rios/TLG_JoinCaptchaBot", # Bot code repository
-    "DEV_PAYPAL" : "https://www.paypal.me/josrios", # Developer Paypal address
-    "DEV_BTC" : "3N9wf3FunR6YNXonquBeWammaBZVzTXTyR", # Developer Bitcoin address
-    "VERSION" : "1.5.1 (14/08/2019)" # Bot version
+
+    # Bot Token (get it from @BotFather)
+    "TOKEN" : "XXXXXXXXX:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+
+    # Languages texts files directory path
+    "LANG_DIR" : SCRIPT_PATH + "/language",
+
+    # Chats directory path
+    "CHATS_DIR" : SCRIPT_PATH + "/data/chats",
+
+    # Directory where create/generate temporary captchas
+    "CAPTCHAS_DIR" : SCRIPT_PATH + "/data/captchas",
+
+    # Chat configurations JSON files
+    "F_CONF" : "configs.json",
+
+    # Initial chat title at Bot start
+    "INIT_TITLE" : "Unknown Chat",
+
+    # Initial chat link at Bot start
+    "INIT_LINK" : "Unknown",
+
+    # Initial language at Bot start
+    "INIT_LANG" : "EN",
+
+    # Initial enable/disable status at Bot start
+    "INIT_ENABLE" : True,
+
+    # Initial captcha solve time (in minutes)
+    "INIT_CAPTCHA_TIME_MIN" : 5,
+
+    # Initial captcha difficult level
+    "INIT_CAPTCHA_DIFFICULTY_LEVEL" : 2,
+
+    # Initial captcha characters mode (nums, hex or ascci)
+    "INIT_CAPTCHA_CHARS_MODE" : "nums",
+
+    # Default time (in mins) to remove self-destruct sent messages from the Bot
+    "T_DEL_MSG" : 5,
+
+    # IANA Top-Level-Domain List (https://data.iana.org/TLD/tlds-alpha-by-domain.txt)
+    "F_TLDS" : "tlds-alpha-by-domain.txt",
+
+    # Regular expression to detect URLs in a string based in TLD domains
+    "REGEX_URLS" : r"((?<=[^a-zA-Z0-9])*(?:https\:\/\/|[a-zA-Z0-9]{{1,}}\.{{1}}|\b)" \
+                   r"(?:\w{{1,}}\.{{1}}){{1,5}}(?:{})\b/?(?!@))",
+
+    # Bot developer
+    "DEVELOPER" : "@JoseTLG",
+
+    # Bot code repository
+    "REPOSITORY" : "https://github.com/J-Rios/TLG_JoinCaptchaBot",
+
+    # Developer Paypal address
+    "DEV_PAYPAL" : "https://www.paypal.me/josrios",
+
+    # Developer Bitcoin address
+    "DEV_BTC" : "3N9wf3FunR6YNXonquBeWammaBZVzTXTyR",
+
+    # Bot version
+    "VERSION" : "1.5.2 (15/08/2019)"
 }
 
+
+# Supported languages list
 TEXT = {
-    'EN' : None,
-    'ES' : None,
-    'CA' : None,
-    'GL' : None,
-    'PT_BR' : None,
-    'ZH_CN' : None
+    "EN" : None, # English
+    "ES" : None, # Spanish
+    "CA" : None, # Catalan
+    "GL" : None, # Galician
+    "PT_BR" : None, # Portuguese (Brasil)
+    "ZH_CN" : None # Chinese (Mainland)
 }
