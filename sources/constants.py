2a1b10050bad2eda11f765998931af5fd16a24f4
J-Rios <jrios.github@gmail.com>
2022-06-07 18:20:39 +0200

    Explicit markdown escape to v2

--- sources/constants.py
+++ sources/constants.py
@@ -10,9 +10,9 @@ Author:
 Creation date:
     09/09/2018
 Last modified date:
-    06/06/2022
+    07/06/2022
 Version:
-    1.26.2
+    1.26.3
 '''
 
 ###############################################################################
@@ -290,7 +290,7 @@ CONST = {
     "DEV_DONATION_ADDR": "https://www.buymeacoffee.com/joincaptchabot",
 
     # Bot version
-    "VERSION": "1.26.2 (06/06/2022)"
+    "VERSION": "1.26.3 (07/06/2022)"
 }
 
 # Supported languages list
